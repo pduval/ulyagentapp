5407994d66f456718134f60cb0d50113828f4d42
Philippe Duval <philippe.duval@qms-software.com>
2015-08-27 05:23:27 +0100

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,100 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+console.log("starting ionic");
+angular.module('agentapp', ['ionic', "angular-hal", "agentapp.controllers"])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.withCredentials = true;
+    })
+    .config(function($stateProvider, $urlRouterProvider) {
+        console.log("configuring router");
+        $urlRouterProvider.otherwise("login");
+        console.log("configuring routes");
+        $stateProvider
+            .state("login", {
+                url:"/login",
+                templateUrl:"templates/login.html",
+                controller:"LoginCtrl",
+                public: true
+            })
+            .state("home", {
+                url:"/home",
+                templateUrl:"templates/tickets.html",
+                controller:"TicketCtrl"
+            })
+            .state("help", {
+                url:"/help",
+                template:"<ion-view view-title='Help'><h1>Help</h1><p>Please help yourself</p></ion-view>"
+            });
+    })
+    .factory("UserInfo", function() {
+        var userData = {};
+        return {
+            isLoggedIn: function() {
+                return userData.username != null;
+            },
+            getUserData: function() {
+                return angular.extend({}, userData);
+            },
+            setUserData: function(data) {
+                angular.extend(userData, data);
+            }
+        };
+    })
+    .factory('RESTService', [ 'halClient', function(halClient) {
+        console.log("creating rest service");
+        
+        var root = halClient.$get("http://10.141.2.140:6543/api/v2");
+        return  {
+            "url": "http://10.141.2.140:6543",
+            "set_url": function(new_root) {
+                this.url = new_root;
+                root = halClient.$get(new_root + "/api/v2");
+                return root;
+            },
+            "start": function() {
+                return root;
+            },
+            'login' : function(username, password) {
+                console.error("Logging in with:", username, password);
+                return root.then(function(resource) {
+                    return resource.$get("uly:app").then(function(app) {
+                        console.log("got app:", app);
+                        return app.$post("uly:signin", { "username":username, "password":password});
+                    });
+                });
+            },
+            'load' : function() {
+                return root.then(function(resource) {
+                    return resource.$get("uly:data");
+                })
+                    .then(function(data) {
+                        return data.$get("uly:ticket", {"embed":1});
+                    });
+            }
+        };
+    }])
+    .run(function($ionicPlatform, $rootScope, $location, UserInfo) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+        console.log("running");
+        $rootScope.$on('$stateChangeStart', function (ev, next, nextparams, curr) {
+            console.log("route change start:", next);
+            if (next.$$route) {
+                var user = UserInfo.getUserData();
+                var isPublic = next.$$route.public;
+                if (!isPublic  && !(user && user.fullname))  { $location.path('/login') }
+            }
+        });
+        
+    });
---
+++ www/js/controllers.js
@@ -0,0 +1,48 @@
+angular.module('agentapp.controllers', ['ionic', "angular-hal"])
+    .controller('TicketCtrl', function($scope, RESTService) {
+        $scope.tickets = $scope.tickets || [];
+        RESTService.load().then(function(ticketcoll) {
+            ticketcoll.$get("item").then(function(items) {
+                console.log("Got results:", items);
+                $scope.tickets = items;
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        }, function(error) { console.error("error:", arguments); });
+    })
+    .controller("LoginCtrl", function($scope, $state, RESTService, UserInfo) {
+        $scope.user_data = $scope.user_data || {
+            "username": "",
+            "password": "",
+            "server":RESTService.url
+        };
+        $scope.login = function() {
+            if ($scope.user_data.server && RESTService.url != $scope.user_data.server) {
+                RESTService.set_url($scope.user_data.server);
+            }
+            RESTService.login($scope.user_data.username, $scope.user_data.password).then(function(app) {
+                if (app.error) {
+                    $scope.error = app.error;
+                }
+                else {
+                    app.$get("uly:user").then(function(user) {
+                        UserInfo.setUserData({
+                            "username": $scope.user_data.username,
+                            "server":$scope.user_data.server,
+                            "email": user.email,
+                            "picture": user.picture,
+                            "fullname": user.fullname,
+                            "roles": user.roles
+                        });
+                    });
+                    $scope.error = "";
+                    //navigate to home
+                    $state.go("home");
+                }
+
+            }, function(error) {
+                $scope.error = "Invalid credentials";
+            });
+        };
+    });
+
+
